43b0bafd3e3a26a176dc165b2e700ff8e555dffa
TJPerson04 <HiPerson929@gmail.com>
2024-07-27 13:08:55 -0400

    24 hour to 12 hour clock conversion

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 CHIME_HOURS = [8, 12, 20]
-CHIME_MIN = 15
+CHIME_MIN = 23
 RESET_HOUR = 23
 
 WESTMINSTER_FILE_PATH = "home/westminster-chimes.wav"
@@ -19,8 +19,15 @@ while True:
             print("Playing a chime for hour: " + str(hour) + " and min: " + str(CHIME_MIN))
             is_played = True
             os.system("aplay " + WESTMINSTER_FILE_PATH)
-            for i in range(hour):
+
+            # Since datetime works on a 24 hour clock, the hour has to be converted to the 12 hour clock for the chimes
+            numRepeat = hour % 12
+            if numRepeat == 0:
+                numRepeat = 12
+
+            for i in range(numRepeat):
                 os.system("aplay " + BELL_CHIME_FILE_PATH)
+    
     if currentHour == RESET_HOUR and currentMin == 0:  # So that the Raspberry Pi is not just constantly on
         print("Rebooting Now")
         # os.system("sudo reboot -n")
