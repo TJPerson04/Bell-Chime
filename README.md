d74c0ac09f241a06d3a4df3447d778038fa91fc8
TJPerson04 <HiPerson929@gmail.com>
2024-07-27 12:10:47 -0400

    Made things more clear

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This is a project for The Sanctuary Museum, a nonprofit museum in Lakewood, Ohio situated in an old church. This project was made to control the timing of the bells so that they ring a specific number of times at specific times of the day.
 This project is made to run on a Raspberry Pi 4 that is connected to the speaker system.
+The Raspberry Pi should also run main.py on boot so that when it reboots it automatically runs the program again.
+
+Currently line 23 in main.py is commented out for safety reasons. In actual application that line would not be commented out.
--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import os
 from datetime import datetime
 
-chimeHours = [8, 11, 20]
-chimeMin = 0
-resetHour = 23
+CHIME_HOURS = [8, 11, 20]
+CHIME_MIN = 0
+RESET_HOUR = 23
+
+WESTMINSTER_FILE_PATH = "home/westminster-chimes.wav"
+BELL_CHIME_FILE_PATH = "home/funeral-bell.wav"
+
 is_played = False
 while True:
     now = datetime.now()
     currentHour = now.hour
     currentMin = now.minute
 
-    for hour in chimeHours:
-        if currentHour == hour and currentMin == chimeMin and not is_played:
+    for hour in CHIME_HOURS:
+        if currentHour == hour and currentMin == CHIME_MIN and not is_played:
             print("Playing a chime for " + str(hour) + ":00")
             is_played = True
-            # os.system("aplay home/westminster-chimes.wav")
+            os.system("aplay " + WESTMINSTER_FILE_PATH)
             for i in range(hour):
                 print("Test")
-                # os.system("aplay home/funeral-bell.wav")
-    if currentHour == resetHour:
+                os.system("aplay " + BELL_CHIME_FILE_PATH)
+    if currentHour == RESET_HOUR:  # So that the Raspberry Pi is not just constantly on
         print("Rebooting Now")
         # os.system("sudo reboot -n")
-    if not (currentHour in chimeHours) or currentMin != chimeMin:
+    if not (currentHour in CHIME_HOURS) or currentMin != CHIME_MIN:
         is_played = False
