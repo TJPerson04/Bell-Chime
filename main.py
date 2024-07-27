df1a97fe493aaaea56298d1a6b9a1a150bd33168
TJPerson04 <HiPerson929@gmail.com>
2024-07-27 12:21:24 -0400

    Minor bug fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from datetime import datetime
 
-CHIME_HOURS = [8, 11, 20]
-CHIME_MIN = 0
+CHIME_HOURS = [8, 12, 20]
+CHIME_MIN = 15
 RESET_HOUR = 23
 
 WESTMINSTER_FILE_PATH = "home/westminster-chimes.wav"
@@ -16,13 +16,12 @@ while True:
 
     for hour in CHIME_HOURS:
         if currentHour == hour and currentMin == CHIME_MIN and not is_played:
-            print("Playing a chime for " + str(hour) + ":00")
+            print("Playing a chime for hour: " + str(hour) + " and min: " + str(CHIME_MIN))
             is_played = True
             os.system("aplay " + WESTMINSTER_FILE_PATH)
             for i in range(hour):
-                print("Test")
                 os.system("aplay " + BELL_CHIME_FILE_PATH)
-    if currentHour == RESET_HOUR:  # So that the Raspberry Pi is not just constantly on
+    if currentHour == RESET_HOUR and currentMin == 0:  # So that the Raspberry Pi is not just constantly on
         print("Rebooting Now")
         # os.system("sudo reboot -n")
     if not (currentHour in CHIME_HOURS) or currentMin != CHIME_MIN:
